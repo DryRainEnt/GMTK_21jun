d4252b960cb39d527d9fd1457204907a3baf2f1b
unengine <unengine@gmail.com>
2021-06-12 23:28:31 +0900

    두트윈 라이브러리 넣기, 레이저 틀 추가

---
+++ GMTK_21jun/Assets/Scripts/Projectiles/LaserCannon.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserCannon : MonoBehaviour
+{
+    public float startDelay;
+    public float shortLaserDelay;
+    public float longLaserDelay;
+
+    public Transform target;
+
+    private void Awake()
+    {
+        StartCoroutine(CoLaunchShortLaser());
+        StartCoroutine(CoLaunchLongLaser());
+    }
+
+    private IEnumerator CoLaunchShortLaser()
+    {
+        yield return new WaitForSeconds(startDelay);
+        var waitSec = new WaitForSeconds(shortLaserDelay);
+        while (gameObject.activeSelf)
+        {
+            yield return waitSec;
+
+
+        }
+    }
+
+    private IEnumerator CoLaunchLongLaser()
+    {
+        yield return new WaitForSeconds(startDelay);
+        var waitSec = new WaitForSeconds(longLaserDelay);
+        while (gameObject.activeSelf)
+        {
+            yield return waitSec;
+
+
+        }
+    }
+}
