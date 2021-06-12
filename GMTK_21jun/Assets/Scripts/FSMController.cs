43ba20ad951278c4c7d710bfd38476ea3289c3d5
unengine <unengine@gmail.com>
2021-06-12 20:23:03 +0900

    보스 패턴 구현 (레이저)
    # Conflicts:
    #	GMTK_21jun/Assets/Prefabs.meta

---
+++ GMTK_21jun/Assets/Scripts/FSMController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSMController<T> where T : Enum
+{
+    private Dictionary<string, FSMState<T>> _fsmStates = new Dictionary<string, FSMState<T>>();
+
+    public FSMState<T> CurrentBehaviour { get; set; }
+
+    public virtual void AddState(FSMState<T> state)
+    {
+        var stateId = state.GetStateId();
+
+        if (!_fsmStates.ContainsKey(stateId))
+        {
+            _fsmStates[stateId] = state;
+            CurrentBehaviour = state;
+        }
+        else
+        {
+            Debug.LogError($"Already added in {GetType().Name}: {state}");
+        }
+    }
+
+    public virtual void RemoveState(FSMState<T> state)
+    {
+        var stateId = state.GetStateId();
+
+        if (_fsmStates.ContainsKey(stateId))
+        {
+            _fsmStates.Remove(stateId);
+        }
+        else
+        {
+            Debug.LogError($"Not exists in {GetType().Name}: {state}");
+        }
+    }
+
+    public virtual void DoTransition(T transition)
+    {
+        var stateId = CurrentBehaviour.GetOutputStateId(transition);
+        var state = _fsmStates[stateId];
+
+        // state.BeforeTransition();
+        CurrentBehaviour = state;
+        // state.AfterTransition();
+    }
+}
---
+++ GMTK_21jun/Assets/Scripts/FSMState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class FSMState<T> where T : Enum
+{
+    protected Dictionary<T, string> _stateMap = new Dictionary<T, string>();
+
+    public abstract void CheckCondition(GameObject actor, GameObject target);
+
+    public abstract void Act(GameObject actor, GameObject target);
+
+    public abstract string GetStateId();
+
+    public virtual string GetOutputStateId(T transition)
+    {
+        if (_stateMap.TryGetValue(transition, out var stateId))
+        {
+            return stateId;
+        }
+        return null;
+    }
+
+    //public abstract void BeforeTransition();
+
+    //public abstract void AfterTransition();
+}
