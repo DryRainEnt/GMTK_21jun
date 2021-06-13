a5629b26f72b6b1b3ad1ead32ef46c869a338808
Bae Junhyeong <bjh6681@outlook.kr>
2021-06-14 00:25:21 +0900

    SwarmCount 구현

---
+++ GMTK_21jun/Assets/Scripts/SwarmCounter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class SwarmCounter : MonoBehaviour
+{
+    public PlayerBehaviour playerBehaviour;
+    Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = playerBehaviour.Swarm.Count.ToString();
+    }
+}
