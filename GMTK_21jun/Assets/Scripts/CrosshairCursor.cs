88371b48437836aecd8a9a79c45da9132e9de7aa
Bae Junhyeong <bjh6681@outlook.kr>
2021-06-13 23:59:35 +0900

    Add GameEndPanel in InGameScene

---
+++ GMTK_21jun/Assets/Scripts/CrosshairCursor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class CrosshairCursor : MonoBehaviour
+{
+    public Sprite onReleaseSprite, onHoldDownSprite;
+    new SpriteRenderer renderer;
+    new Camera camera;
+
+    private void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+            renderer.sprite = onHoldDownSprite;
+        else
+            renderer.sprite = onReleaseSprite;
+
+        transform.position = (Vector2)camera.ScreenToWorldPoint(Input.mousePosition);
+        
+    }
+}
---
+++ GMTK_21jun/Assets/Scripts/GameEndPanel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEndPanel : MonoBehaviour
+{
+    public PlayTimeRecorder timeRecorder;
+
+    private void Awake()
+    {
+        timeRecorder.StopTime();
+    }
+}
