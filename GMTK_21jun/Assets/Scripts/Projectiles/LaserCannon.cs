d4adcc02dff94d1209a7c1cb120becab72239d9d
unengine <unengine@gmail.com>
2021-06-13 23:07:51 +0900

    레이저 로직 보완 및 충돌판정 구현

--- GMTK_21jun/Assets/Scripts/Projectiles/LaserCannon.cs
+++ GMTK_21jun/Assets/Scripts/Projectiles/LaserCannon.cs
@@ -1,22 +1,27 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using System.Linq;
+using System;
 
 public class LaserCannon : MonoBehaviour
 {
     public float startDelay;
 
+    public bool disableShortLaser;
     public float shortLaserCooldown;
     public float shortLaserDelay;
     public float shortLaserDuration;
-    public float shortlaserWidth;
+    public float shortLaserWidth;
+    public int shortLaserDamage;
 
+    public bool disableLongLaser;
     public float longLaserCooldown;
     public float longLaserDelay;
+    public float longLaserLifeTime;
     public float longLaserDuration;
     public float longLaserWidth;
+    public int longLaserDamage;
 
     public float laserLength = 100;
 
@@ -26,8 +31,14 @@ public class LaserCannon : MonoBehaviour
 
     private void Awake()
     {
-        StartCoroutine(CoLaunchShortLaser());
-        StartCoroutine(CoLaunchLongLaser());
+        if (!disableShortLaser)
+        {
+            StartCoroutine(CoLaunchShortLaser());
+        }
+        if (!disableLongLaser)
+        {
+            StartCoroutine(CoLaunchLongLaser());
+        }
     }
 
     private IEnumerator CoLaunchShortLaser()
@@ -47,29 +58,43 @@ public class LaserCannon : MonoBehaviour
             var spriteBorder = renderer.sprite.border;
             var width = renderer.sprite.rect.width - (spriteBorder.x + spriteBorder.z);
             var direction = SetLaserTransform(laserObject, laserLength, ppu);
-            renderer.size = new Vector2(1f, laserLength);
 
-            renderer.color = Color.white;
+            renderer.size = new Vector2(1f, laserLength);
+            renderer.color = Color.red;
+
+            var collider = laserObject.GetComponent<BoxCollider2D>();
+            var colliderSizeX = collider.size.x;
+            collider.size = new Vector2(colliderSizeX, laserLength);
+            collider.enabled = false;
 
             laserObject.transform.localScale = new Vector3(0f, 1f, 0f);
-            laserObject.transform.DOScaleX(shortlaserWidth, shortLaserDelay);
+            laserObject.transform.DOScaleX(shortLaserWidth, shortLaserDelay);
             yield return new WaitForSeconds(shortLaserDelay);
 
-            var size = new Vector2(laserLength, shortlaserWidth * (width / ppu));
-            var isInterrupted = CheckLaserInterrupted(
+            var size = new Vector2(laserLength, shortLaserWidth * (width / ppu));
+            var blockObject = DealDamage(
                 laserObject.transform.position,
                 size,
                 direction);
-            if (isInterrupted)
+            if (blockObject)
             {
-                Debug.Log("interrupt");
-                var length = Vector2.Distance(target.position, transform.position);
-                laserObject.transform.position = transform.position + (direction * length * 0.5f);
-                laserObject.transform.localScale = new Vector3(shortlaserWidth, 1f, 1f);
-                renderer.size = new Vector2(1f, length);
+                var collector = blockObject.GetComponent<VirtualCollectorBehaviour>();
+                var isAlive = collector.GetDamage(shortLaserDamage);
+
+                if (isAlive)
+                {
+                    var length = Vector2.Distance(blockObject.position, transform.position);
+                    laserObject.transform.position = transform.position + (direction * length * 0.5f);
+                    laserObject.transform.localScale = new Vector3(shortLaserWidth, 1f, 1f);
+                    renderer.size = new Vector2(1f, length);
+                    collider.size = new Vector2(colliderSizeX, length);
+                }
             }
-            renderer.color = Color.red;
+
+            collider.enabled = true;
+            renderer.color = Color.white;
             renderer.DOFade(0f, shortLaserDuration);
+            laserObject.transform.DOScaleX(0f, shortLaserDuration);
             yield return new WaitForSeconds(shortLaserDuration);
             laserObject.SetActive(false);
         }
@@ -87,47 +112,66 @@ public class LaserCannon : MonoBehaviour
                 continue;
             }
 
-
             var renderer = laserObject.GetComponent<SpriteRenderer>();
             var ppu = renderer.sprite.pixelsPerUnit;
             var spriteBorder = renderer.sprite.border;
             var width = renderer.sprite.rect.width - (spriteBorder.x + spriteBorder.z);
             var direction = SetLaserTransform(laserObject, laserLength, ppu);
-            renderer.size = new Vector2(1f, laserLength);
 
-            renderer.color = Color.white;
+            renderer.size = new Vector2(1f, laserLength);
+            renderer.color = Color.red;
+
+            var collider = laserObject.GetComponent<BoxCollider2D>();
+            var colliderSizeX = collider.size.x;
+            collider.size = new Vector2(colliderSizeX, laserLength);
+            collider.enabled = false;
+
             laserObject.transform.localScale = new Vector3(0f, 1f, 0f);
             laserObject.transform.DOScaleX(longLaserWidth, longLaserDelay);
             yield return new WaitForSeconds(longLaserDelay);
 
             var size = new Vector2(laserLength, longLaserWidth * (width / ppu));
-            var isInterrupted = CheckLaserInterrupted(
+            var blockObject = DealDamage(
                 laserObject.transform.position,
                 size,
                 direction);
-            if (isInterrupted)
+            if (blockObject)
             {
-                var length = Vector2.Distance(target.position, transform.position);
-                laserObject.transform.position = transform.position + (direction * length * 0.5f);
-                laserObject.transform.localScale = new Vector3(longLaserWidth, 1f, 1f);
-                renderer.size = new Vector2(1f, length);
+                var collector = blockObject.GetComponent<VirtualCollectorBehaviour>();
+                var isAlive = collector.GetDamage(longLaserDamage);
+
+                if (isAlive)
+                {
+                    var length = Vector2.Distance(blockObject.position, transform.position);
+                    laserObject.transform.position = transform.position + (direction * length * 0.5f);
+                    laserObject.transform.localScale = new Vector3(longLaserWidth, 1f, 1f);
+                    renderer.size = new Vector2(1f, length);
+                    collider.size = new Vector2(colliderSizeX, length);
+                }
             }
-            renderer.color = Color.red;
+
+            collider.enabled = true;
+            renderer.color = Color.white;
+            yield return new WaitForSeconds(longLaserLifeTime);
+
             renderer.DOFade(0f, longLaserDuration);
+            laserObject.transform.DOScaleX(0f, longLaserDuration);
             yield return new WaitForSeconds(longLaserDuration);
             laserObject.SetActive(false);
         }
     }
 
-    private bool CheckLaserInterrupted(Vector2 pos, Vector2 size, Vector2 direction)
+    private Transform DealDamage(Vector2 pos, Vector2 size, Vector2 direction)
     {
         var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         if (angle < 0) angle += 360f;
 
         size = new Vector3(size.x, size.y, 1f);
-        return Physics2D.OverlapBoxAll(pos, size, angle)
-            .Where(x => x.CompareTag("BlockLaser"))
-            .Any();
+
+        var blockObject = Physics2D.OverlapBoxAll(pos, size, angle)
+            .FirstOrDefault(x => x.CompareTag("BlockLaser"));
+
+        return blockObject is null ? null : blockObject.transform;
     }
 
     private Vector3 SetLaserTransform(GameObject laserObject, float laserLength, float ppu)
