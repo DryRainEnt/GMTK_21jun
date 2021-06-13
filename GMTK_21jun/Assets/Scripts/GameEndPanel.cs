47894b2b3c1cd1a6b17fb6d24aba8eebc31c4ba5
Bae Junhyeong <bjh6681@outlook.kr>
2021-06-14 00:09:58 +0900

    GameEndPanel 기능추가

--- GMTK_21jun/Assets/Scripts/GameEndPanel.cs
+++ GMTK_21jun/Assets/Scripts/GameEndPanel.cs
@@ -6,8 +6,21 @@ public class GameEndPanel : MonoBehaviour
 {
     public PlayTimeRecorder timeRecorder;
 
+    public GameObject clearPanel;
+
     private void Awake()
     {
         timeRecorder.StopTime();
+    }
+
+    public void GameCleared()
+    {
+        gameObject.SetActive(true);
+        clearPanel.SetActive(true);
+    }
+
+    public void GameOvered()
+    {
+        gameObject.SetActive(true);
     }
 }
